57c2116cd65ad8f13da7fe403dc37e0d9e9b9a42
stefano.dinca@byrontec.com <stefano.dinca@byrontec.com>
2025-08-12 18:28:47 +0200

    GET /auth/confirm endpoint

--- src/main/java/com/dinkybell/ecommerce/controllers/UserAthenticationController.java
+++ src/main/java/com/dinkybell/ecommerce/controllers/UserAthenticationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.dinkybell.ecommerce.dtos.RegisterRequestDTO;
 import com.dinkybell.ecommerce.services.UserAuthenticationService;
@@ -23,27 +24,13 @@ public class UserAthenticationController {
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody @Valid RegisterRequestDTO request) {
         // Call the service to register the user
-        System.out.println(request.getEmail());
-        System.out.println(request.getPassword());
         return userAuthenticationService.registerUser(request.getEmail(), request.getPassword());
     }
 
-    @Autowired
-    private JavaMailSender mailSender; // or MailSender mailSender;
-
-    @GetMapping("/test-email")
-    public ResponseEntity<?> testEmail() {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("support@dinkybell.com");
-            message.setTo("stefano.dinca@fastwebnet.it");
-            message.setSubject("Test");
-            message.setText("Email configuration working!");
-            mailSender.send(message);
-            return ResponseEntity.ok("Email sent successfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Email failed: " + e.getMessage());
-        }
+    @GetMapping("/confirm-email")
+    public ResponseEntity<?> confirmEmail(@RequestParam String token) {
+        System.out.println("Confirming email with token: " + token);
+        return userAuthenticationService.confirmEmail(token);
     }
 
 }
